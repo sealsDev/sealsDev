e394b33f02aceed0797945356bf825ccdbe53259
Valera <61586353+alexnoganov@users.noreply.github.com>
2020-02-29 21:07:47 +0200

    New

---
+++ Langtime/include/header/header.php
@@ -0,0 +1,26 @@
+<header>
+            <div class="header__inner">
+                <div class="header__logo"><a href="Myproject.html" style="text-decoration: none; color: #404040;">langtime</a></div>
+                <nav class="header__menu">
+                    <ul class="topmenu">
+                        <li><a class="header__link">Курсы</a>
+                            <ul class="submenu">
+                                <li><a class="header__link" href="">Немецкий</a></li>
+                                <li><a class="header__link" href="">Французкий</a></li>
+                                <li><a class="header__link" href="">Украинский</a></li>
+                                <li><a class="header__link sub_eng">Английский</a>
+                                    <ul class="submenu__eng">
+                                        <li><a class="header__link" href="Courses_Eng_teenager.html">Взрослый</a></li>
+                                        <li><a class="header__link" href="Courses_Eng_chield.html">Детский</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a class="header__link" href="#w33">О нас</a></li>
+                        <li><a class="header__link" href="#miracle">Контакты</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="header__link__phone"><a href="tel:+380674974066" style="text-decoration: none;
+            color: #424242;">+38(067)497-40-66</a></div>
+        </header>
