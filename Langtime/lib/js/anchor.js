b561ba506243b44419790f1b42b03c0c818189cc
Valera <61586353+alexnoganov@users.noreply.github.com>
2020-03-01 21:10:26 +0200

    Update anchor.js

--- Langtime/lib/js/anchor.js
+++ Langtime/lib/js/anchor.js
@@ -1,18 +1,14 @@
- function up() {
-     var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
-     if (top > 0) {
-         window.scrollBy(0, ((top + 100) / -25));
-         t = setTimeout('up()', 20);
-     } else clearTimeout(t);
-     return false;
- }
+var btn = $('#back-top');
 
- $(document).ready(function() {
-    $('a[href*="#"]').on('click', function (e) {
-        e.preventDefault();
-       
-        $('html, body').animate({
-          scrollTop: $($(this).attr('href')).offset().top
-        }, 1300, 'linear');
-      });
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
 });
