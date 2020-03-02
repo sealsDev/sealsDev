698d8d337f6c72a6c6ed7f54e8f117a67129669d
Valera <61586353+alexnoganov@users.noreply.github.com>
2020-03-02 19:50:56 +0200

    `

--- Langtime/lib/js/anchor.js
+++ Langtime/lib/js/anchor.js
@@ -1,14 +1,52 @@
-var btn = $('#back-top');
+function scrollTop() {
+  var btn = $('#back-top');
 
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
 
 btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '300');
 });
+}
+
+function scrollToAnchor() {
+  $(document).ready(function(){
+        $("#nav").on("click","a", function (event) {
+            //отменяем стандартную обработку нажатия по ссылке
+            event.preventDefault();
+            //забираем идентификатор бока с атрибута href
+            var id  = $(this).attr('href'),
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+                top = $(id).offset().top - 150;
+            //анимируем переход на расстояние - top за 1500 мс
+            $('body,html').animate({scrollTop: top}, 300);
+        });
+    });
+
+    $(document).ready(function(){
+      $("#goToForm").on("click","div", function (event) {
+          //отменяем стандартную обработку нажатия по ссылке
+          event.preventDefault();
+          //забираем идентификатор бока с атрибута href
+          var id  = $('#goToFormLink').attr('href'),
+          //узнаем высоту от начала страницы до блока на который ссылается якорь
+              top = $(id).offset().top - 180;
+          //анимируем переход на расстояние - top за 1500 мс
+          $('body,html').animate({scrollTop: top}, 300);
+      });
+  });
+    
+}
+
+
+$('.dropdown-el').click(function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  $(this).toggleClass('expanded');
+  $('#'+$(e.target).attr('for')).prop('checked',true);
+});
+$(document).click(function() {
+  $('.dropdown-el').removeClass('expanded');
+});
+
+scrollToAnchor();
+scrollTop();
